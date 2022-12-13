c1d25183a8555a85dc7e72470bfd5df5715879b4
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-13 17:18:09 -0500

    Change url of backend application

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const authApi = axios.create({
-    //baseURL: 'http://localhost:8080/api/auth',
-    baseURL: 'https://api-v2-hairroom.herokuapp.com/api/auth',
+    // baseURL: 'http://localhost:8082/api/auth',
+    // baseURL: 'https://api-v2-hairroom.herokuapp.com/api/auth',
+    baseURL: 'https://api-v2-hairroom.vercel.app/api/auth',
 })
 
 export default authApi;
--- src/api/ordersApi.ts
+++ src/api/ordersApi.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const ordersApi = axios.create({
-    //baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://api-v2-hairroom.herokuapp.com/api',
+    // baseURL: 'http://localhost:8082/api',
+    // baseURL: 'https://api-v2-hairroom.herokuapp.com/api',
+    baseURL: 'https://api-v2-hairroom.vercel.app/api',
 })
 
 export default ordersApi;
